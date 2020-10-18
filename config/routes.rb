b1d6c60c377bf32615dd785d031fc8aaa861c249
Annie Puygrenier aboucal9@gmail.com <aboucal9@gmail.com>
2020-10-18 13:29:29 +0200

    added about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'pages' => 'pages#home'
   root 'pages#home'
+  get 'about' => 'pages#about', as: 'about'
 end
