deee4b0131b275012a90099aae581a1b7d13c9c2
Annie Puygrenier aboucal9@gmail.com <aboucal9@gmail.com>
2020-10-18 20:37:24 +0200

    exercise over

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -3,4 +3,22 @@ class SignupsController < ApplicationController
     def new
         @signup = Signup.new
     end
+
+    def thanks
+    end
+
+    def create
+        @signup = Signup.new(signup_params)
+        if @signup.save
+          redirect_to thanks_path
+        else
+          render :new
+        end
+    end
+    
+    private
+    
+    def signup_params
+        params.require(:signup).permit(:firstname, :email)
+    end
 end
